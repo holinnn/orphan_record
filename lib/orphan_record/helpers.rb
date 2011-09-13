508e10be53ea64d5b9c77f5460d1e05771ea484a
Aurélien AMILIN <aurelien.amilin@gmail.com>
2011-09-13 15:22:14 +0200

    replace empty href

--- lib/orphan_record/helpers.rb
+++ lib/orphan_record/helpers.rb
@@ -4,7 +4,7 @@ module OrphanRecord
   module Helpers
     def url_for(options = {})
       if options.is_a?(::ActiveRecord::Base) && options.adopter?
-        nil
+        '#'
       else
         super
       end
